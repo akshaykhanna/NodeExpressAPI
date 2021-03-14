7e0819acdc92dcde328046f860a360d28266c99a
akshaykhanna <akshay9911102163@gmail.com>
2021-03-14 15:53:30 +0530

    Create bootcamps get, post, put & delete end points

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dovenv = require('dotenv');
+const { json } = require('express');
 
 dovenv.config({ path: './config/config.env' });
 
@@ -11,3 +12,38 @@ app.listen(
   PORT,
   () => `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
 );
+
+app.get('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({
+    success: true,
+    msg: 'Show all bootCamps'
+  })
+})
+
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({
+    success: true,
+    msg: `Get bootCamp ${req.params.id}`
+  })
+})
+
+app.post('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({
+    success: true,
+    msg: `Created new bootCamp`
+  })
+})
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({
+    success: true,
+    msg: `Update bootCamp ${req.params.id}`
+  })
+})
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({
+    success: true,
+    msg: `Delete bootCamp ${req.params.id}`
+  })
+})
