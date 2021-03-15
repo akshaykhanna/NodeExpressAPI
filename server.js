2a14f76ee53d9f6fe603dde3138207d8b0fbfd6a
akshaykhanna <akshay9911102163@gmail.com>
2021-03-15 08:37:00 +0530

    Use thirdParty middleware morgan as logger

--- server.js
+++ server.js
@@ -3,14 +3,16 @@ const dovenv = require('dotenv');
 const logger = require('./middleware/logger');
 // router files
 const bootCamps = require('./router/bootcamps')
-
+var morgan = require('morgan');
 dovenv.config({ path: './config/config.env' });
 
 const app = express();
 
 
 // Set middleware
-app.use(logger)
+if(process.env.NODE_ENV='development') {
+  app.use(morgan('dev'));
+}
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootCamps);
